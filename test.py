0f6a85220810ffeeb4c9862af7bfaf40ef9101c5
sebastian-montero <smonteroparis@icloud.com>
2020-11-17 16:06:15 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,42 @@
+# %%
+from pigging import Tracker, importConnectors, exportConnectors
+
+# %%
+tracker = Tracker(log_path="./test.log", lock_path="./test.lock")
+tracker.startup()
+
+tracker.logger.info()
+
+import_conn = importConnectors(
+    "/home/sebastianmontero/Credentials/gcp_personal_sebastian.json"
+)
+export_conn = exportConnectors(
+    "/home/sebastianmontero/Credentials/gcp_personal_sebastian.json"
+)
+# %%
+
+data = import_conn.google_big_query(
+    "SELECT * FROM Salesforce.beamery_companies", "beamery-data"
+)
+data.head()
+# %%
+
+export_conn.google_big_query(
+    data, "Salesforce.beamery_companies", "beamery-data", "replace"
+)
+
+# %%
+tracker.log("hi", "INFO")
+try:
+    print("hello")
+except Exception as e:
+    tracker.log_exception(e)
+
+tracker.log("this is a warning", "WARN")
+tracker.log("this is an erroor", "ERROR")
+
+
+# %%
+tracker.close()
+
+# %%
