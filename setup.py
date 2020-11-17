27c773a4801fa1011ab1c747087d41332b1974d8
sebastian-montero <smonteroparis@icloud.com>
2020-11-17 16:10:09 +0000

    added tests and added URL to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="ptolemaios",
+    name="pigging",
     version="0.0.1",
-    description="My private package from private github repo",
-    url="git@github.com:rfschubert/ptolemaios-sdk-package.git",
-    author="Raphael Schubert",
-    author_email="raphael.schubert@digitalbankscorp.com",
+    description="Data engineering toolkit to track ETL pipelines and improve cloud connection reliability",
+    url="https://github.com/BeameryEdge/pigging",
+    author="Sebastian Montero",
+    author_email="sebastian.montero@beamery.com",
     license="unlicense",
-    packages=["ptolemaios"],
+    packages=["pigging"],
     zip_safe=False,
 )
