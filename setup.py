d07a2de3b91666c187df3bc88c298763e20a910d
sebastian-montero <smonteroparis@icloud.com>
2020-11-17 23:13:52 +0000

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="pigging",
     version="0.0.1",
     description="Data engineering toolkit to track ETL pipelines and improve cloud connection reliability",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/BeameryEdge/pigging",
     author="Sebastian Montero",
     author_email="sebastian.montero@beamery.com",
-    license="unlicense",
-    packages=["pigging"],
-    zip_safe=False,
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    classifiers=[ 
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
+
+
