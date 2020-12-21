c04ad0d0b3f96312934e34b5ad6eff75ea9ccf09
sebastian-montero <smonteroparis@icloud.com>
2020-12-21 15:50:16 +0000

    added GS connector

--- pigging/connectors.py
+++ pigging/connectors.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from retry import retry
 from google.oauth2 import service_account
+import gspread
+import gspread_dataframe as gd
 
 
 class googleBigQueryConnector(object):
@@ -24,26 +26,43 @@ class googleBigQueryConnector(object):
             )
         return(import_gcp_data(query, project_id))
 
-    def export_data(self, dataframe, table_destination, project_id, if_exists):
+    def export_data(self, dataframe, table_destination, project_id, location, if_exists):
         @retry(tries=self.retry_tries, delay=self.retry_delay)
-        def export_gcp_data(dataframe, table_destination, project_id, if_exists):
+        def export_gcp_data(dataframe, table_destination, project_id, location, if_exists):
             dataframe.to_gbq(
                 destination_table=table_destination,
                 project_id=project_id,
                 if_exists=if_exists,
+                location=location,
                 credentials=self.google_big_query_credentials,
             )
 
-        export_gcp_data(dataframe, table_destination, project_id, if_exists)
+        export_gcp_data(dataframe, table_destination,
+                        project_id, location, if_exists)
         print("Successfully exported data to Google BigQuery")
 
 
 class googleSheetsConnector(object):
-    def __init__(self):
-        ...
+    def __init__(self, google_sheets_credentials, retry_tries=3, retry_delay=3):
+        self.retry_tries = retry_tries
+        self.retry_delay = retry_delay
 
-    def import_data(self):
-        ...
+        self.service_account_loaded = (
+            service_account.Credentials.from_service_account_file(
+                google_sheets_credentials
+            )
+        )
+        print("Successfully started Google Sheets client")
 
-    def export_data(self):
-        ...
+    def export_data(self, dataframe, worksheet_id, worksheet_name):
+
+        client = gspread.authorize(self.service_account_loaded.with_scopes(["https://spreadsheets.google.com/feeds",
+                                                                            "https://www.googleapis.com/auth/drive"]))
+
+        @retry(tries=self.retry_tries, delay=self.retry_delay)
+        def export_gs_data(df, wid, wname):
+            ws_1 = client.open_by_key(wid).worksheet(wname)
+            gd.set_with_dataframe(worksheet=ws_1, dataframe=df, resize=True)
+
+        export_gs_data(dataframe, worksheet_id, worksheet_name)
+        print("Successfully exported data to Google Sheets")
--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -1,9 +1,14 @@
 import pytest
-from pigging.connectors import googleBigQueryConnector
+from pigging.connectors import googleBigQueryConnector, googleSheetsConnector
 import os
 import warnings
+import pandas as pd
 
+### Credentials ###
 CREDENTIALS_PATH = os.environ.get('CREDENTIALS_PATH')
+
+
+### Google Big Query ###
 SELECT_QUERY = os.environ.get('SELECT_QUERY')
 PROEJCT_ID = os.environ.get('PROEJCT_ID')
 TABLE_DESTINATION = os.environ.get('TABLE_DESTINATION')
@@ -22,6 +27,26 @@ class TestGoogleBigQueryConnector(object):
 
     def test_data_export_successful(self, capsys):
         df = gbq_connector.import_data(SELECT_QUERY, PROEJCT_ID)
-        gbq_connector.export_data(df, TABLE_DESTINATION, PROEJCT_ID, 'replace')
+        gbq_connector.export_data(
+            df, TABLE_DESTINATION, PROEJCT_ID, "EU", 'replace')
         captured = capsys.readouterr()
         assert captured.out == "Successfully exported data to Google BigQuery\n"
+
+
+### Google Sheets ###
+WORKSHEET_ID = os.environ.get('WORKSHEET_ID')
+WORKSHEET_NAME = os.environ.get('WORKSHEET_NAME')
+DF = pd.DataFrame(["test value"], columns=['Test col'])
+gs_connector = googleSheetsConnector(CREDENTIALS_PATH)
+
+
+class TestGoogleSheetsConnector(object):
+    def test_connected(self, capsys):
+        gs_connector = googleSheetsConnector(CREDENTIALS_PATH)
+        captured = capsys.readouterr()
+        assert captured.out == "Successfully started Google Sheets client\n"
+
+    def test_data_export_successful(self, capsys):
+        gs_connector.export_data(DF, WORKSHEET_ID, WORKSHEET_NAME)
+        captured = capsys.readouterr()
+        assert captured.out == "Successfully exported data to Google Sheets\n"
